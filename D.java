1e12a3cef8f6aee1df7863c973705e398ad2da50
Pradip-Belgavkar <pbelgavkar394@gmail.com>
2022-10-16 14:06:22 +0530

    dded

--- D.java
+++ D.java
@@ -14,4 +14,8 @@ public class D {
 git config --global user.name "Durgesh"
 git config --global user.email "learncodewithdurgesh@gmail.com"
 
+
+git remote add origin https://github.com/Pradip-Belgavkar/github.git
+git branch -M main
+git push -u origin main
 */
